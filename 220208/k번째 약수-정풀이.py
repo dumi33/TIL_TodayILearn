4a4207f0c844e6949a448bcf850282cebd32555e
dumi33 <75162315+dumi33@users.noreply.github.com>
2022-02-08 14:33:59 +0900

    Create k번째 약수-정풀이.py

---
+++ 220208/k번째 약수-정풀이.py
@@ -0,0 +1,11 @@
+import sys
+#sys.stdin = open("input.txt", "rt") 
+cnt = 0
+n, k = map(int,input().split())
+for i in range(1,n+1) :
+    if n%i == 0 :
+        cnt += 1
+    if cnt == k :
+        print(i)
+        break
+else : print(-1)
