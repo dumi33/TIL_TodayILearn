b192e60aa19aa2a2abd232b5772877fb636943be
dumi33 <75162315+dumi33@users.noreply.github.com>
2022-02-14 23:52:00 +0900

    Create 치킨배달.py

---
+++ 220214/치킨배달.py
@@ -0,0 +1,23 @@
+from itertools import combinations
+
+n,m = map(int,input().split())
+graph = [list(map(int,input().split())) for _ in range(n)]
+
+house = []
+chicken = []
+for i in range(n) :
+    for j in range(n) :
+        if graph[i][j] == 1 : house.append([i,j])
+        elif graph[i][j] == 2 : chicken.append([i,j])
+
+ 
+ans = sys.maxsize
+for ch_addr in combinations(chicken, m) :
+    city_chi = 0
+    for h in house:
+        chick_dis = sys.maxsize
+        for j in range(m) :
+            chick_dis = min(chick_dis, abs(ch_addr[j][0] - h[0]) + abs(ch_addr[j][1] - h[1]))
+        city_chi += chick_dis
+    ans = min(ans, city_chi)
+print(ans)
