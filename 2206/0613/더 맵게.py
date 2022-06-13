9ffed9f70ba606b2e1ab2144a9aee078d608cfba
dumi33 <75162315+dumi33@users.noreply.github.com>
2022-06-13 22:14:34 +0900

    Create 더 맵게.py

---
+++ 2206/0613/더 맵게.py
@@ -0,0 +1,12 @@
+import heapq 
+def solution(scoville, K):
+    answer = 0
+    heapq.heapify(scoville)
+    while len(scoville)>1 :
+        Min = heapq.heappop(scoville)
+        Min2 = heapq.heappop(scoville)
+        heapq.heappush(scoville,Min + Min2*2)
+        answer+=1 
+        if scoville[0] > K :
+            return answer
+    if scoville[0] < K : return -1
