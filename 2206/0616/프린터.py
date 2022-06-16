545f3614865343789a183e751cb262e0840f6ccd
dumi33 <75162315+dumi33@users.noreply.github.com>
2022-06-16 22:41:22 +0900

    Create 프린터.py

---
+++ 2206/0616/프린터.py
@@ -0,0 +1,14 @@
+from collections import deque 
+
+def solution(priorities, location):
+    answer = 0
+    d = deque([v,i] for i,v in enumerate(priorities))
+
+    while d :
+        tmp = d.popleft()
+        if d and tmp[0] >= max(d)[0] :
+            answer+=1 
+            if tmp[1] == location :
+                return answer
+        else :
+            d.append(tmp)
