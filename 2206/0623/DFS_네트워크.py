f0f19c92d237c5e275934ea9e427e0f0e4872ed1
dumi33 <75162315+dumi33@users.noreply.github.com>
2022-06-23 23:44:43 +0900

    Create DFS_네트워크.py

---
+++ 2206/0623/DFS_네트워크.py
@@ -0,0 +1,15 @@
+def solution(n, computers):
+    answer = 0
+    vis = [0 for i in range(n)]
+    
+    def dfs(now) :
+        vis[now] = 1
+        for new in range(n) :
+            if computers[now][new] and vis[new] ==0 :
+                dfs(new)
+    
+    for i in range(n) :
+        if vis[i] == 0 :
+            dfs(i)
+            answer +=1 
+    return answer
