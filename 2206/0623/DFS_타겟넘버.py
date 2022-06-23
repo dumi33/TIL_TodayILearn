30bd70a26e08601a7d49f998a1c53ac05e6f58c4
dumi33 <75162315+dumi33@users.noreply.github.com>
2022-06-23 23:44:23 +0900

    Create DFS_타겟넘버.py

---
+++ 2206/0623/DFS_타겟넘버.py
@@ -0,0 +1,12 @@
+def solution(numbers, target):
+    answer = 0
+    def dfs(idx,val) :
+        nonlocal answer 
+        if idx == len(numbers) :
+            if val == target :
+                answer += 1
+        else :
+            dfs(idx+1, val+numbers[idx])
+            dfs(idx+1, val-numbers[idx])
+    dfs(0,0)
+    return answer
